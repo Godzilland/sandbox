9c9bd4bd829faf205c6ccdf647822a20ecefedac
matevz <matevz@debeli.com>
2018-02-18 17:45:59 +0100

    kreiranje manjse karte, iskanje sosedov

--- table/miza.py
+++ table/miza.py
@@ -18,7 +18,23 @@ class Island:
         self.cards = {}
 
     def add_land_card(self, x, y, id):
+        """ add_land_card - uporablja se samo pri kreiranju mize -
+        postavlja otok"""
         self.cards[(x, y)] = LandCard(id)
+
+    def sosedje(self, x, y, r=1, explorer=False):
+        """ sosedje - vrne listo tuplejev v radiju
+         r rabi navigator, ki lahko premakne nekoga za 2"""
+        lista = []
+        if (x, y - 1) in island.cards.keys():
+            lista.append((x, y - 1))
+        if (x - 1, y) in island.cards.keys():
+            lista.append((x - 1, y))
+        if (x + 1, y) in island.cards.keys():
+            lista.append((x + 1, y))
+        if (x, y + 1) in island.cards.keys():
+            lista.append((x, y + 1))
+        return lista
 
 
 island = Island()
@@ -27,9 +43,22 @@ island.add_land_card(1, 0, 1)
 island.add_land_card(2, 0, 5)
 island.add_land_card(0, 1, 9)
 island.add_land_card(1, 1, 2)
+island.add_land_card(2, 1, 12)
+island.add_land_card(3, 1, 10)
 
-print(island.cards)
+print(island.cards, flush=True)
 
+# print(island.cards[(2, 0)], flush=True)
+
+for x, y in island.cards.keys():
+    print(island.cards[(x, y)])
+
+if (1, 0) in island.cards.keys():
+    print("veljavna koordinata")
+else:
+    print ("neveljavna koordinata")
+
+print(island.sosedje(1, 1))
 
 
 
